f87b1a6258bd6559595692bb4dfe39d24dc354fc
jan-home <42248628+navarrojandg@users.noreply.github.com>
2020-10-29 09:53:35 -0700

    feat/ added readme

---
+++ README.md
@@ -0,0 +1,2 @@
+# Floyd Warshall Algorithm
+## All Pairs Shortest Path
--- index.js
+++ index.js
@@ -1,15 +1,6 @@
 const {FloydWarshall} = require('./lib/floydWarshall');
 const Matrix = require('./lib/matrix/index');
 
-// const array = [
-//   [0,3,8,null,-4],
-//   [null,0,null,1,7],
-//   [null,4,0,null,null],
-//   [2,null,-5,0,null],
-//   [null,null,null,6,0],
-// ];
-
-// exam problem
 const array = [
   [0,3,-6,null,-4],
   [null,0,null,1,7],
